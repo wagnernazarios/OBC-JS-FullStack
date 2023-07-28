6488f00b40d68f94536ff597ad2a50f9c8a324f6
wagnernazarios <wagnernazario124@gmail.com>
2023-07-28 07:24:33 -0300

    add and remove items in array

--- JS-I/array/trabalhandoComArray/index.js
+++ JS-I/array/trabalhandoComArray/index.js
@@ -8,3 +8,17 @@ const arr = [
   "Legolas",
   "Gimli",
 ];
+//adicionando elemento no final do array
+arr.push("Sauron");
+console.log(arr);
+//adicionando elemento no começo do array
+arr.unshift("Golum");
+let tamanho = arr.unshift("Golum");
+console.log(arr);
+console.log(tamanho);
+
+//removendo elemento no final do array
+const ultimoElemento = arr.pop();
+
+console.log(ultimoElemento);
+console.log(arr);
