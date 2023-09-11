28610b8ae6072eccb9071234caa7ae86bc729c8d
wagnernazarios <wagnernazario124@gmail.com>
2023-09-11 19:56:42 -0300

    starting anonimous function class

---
+++ JS-I/funcoes/funcoesAnonimas.js
@@ -0,0 +1,13 @@
+function somar(a, b) {
+  return a + b;
+}
+
+let operação = somar;
+
+console.log(operação(4, 5));
+
+operação = function (a, b) {
+  return a - b;
+};
+
+console.log(operação(4, 5));
